e0d5ce489e628ff637a7c86f25afc5d098f57e7d
Mary <Mary.ruelas06@gmail.com>
2026-01-31 16:58:06 -0600

    new

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,11 +2,10 @@ import "./ItemCard.css";
 import React, { useContext } from "react";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-  const currentUser = useContext(CurrentUserContext);
-  const isLiked = currentUser && item.likes?.some(id => id === currentUser._id);
-  const handleLike = () => {
-    onCardLike({ id: item._id, isLiked });
-  };
+const currentUser = useContext(CurrentUserContext);
+const isLiked = currentUser && item.likes?.some((id) => id === currentUser._id);
+const handleLike = () => {
+  onCardLike({ id: item._id, isLiked });
   return (
     <li className="card">
       <h2 className="card__title">{item.name}</h2>
@@ -23,5 +22,6 @@ import CurrentUserContext from "../../contexts/CurrentUserContext";
       )}
     </li>
   );
+};
 
 export default ItemCard;
