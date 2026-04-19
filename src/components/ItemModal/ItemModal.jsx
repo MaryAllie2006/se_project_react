c4591524a1039d2da8bc0a6ab63bba81aa005768
Mary <Mary.ruelas06@gmail.com>
2026-04-19 13:18:55 -0500

    update

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -7,7 +7,7 @@ function ItemModal({ activeModal, onClose, card, onDeleteItem }) {
   const isOwn = currentUser && card?.owner === currentUser._id;
   
   return (
-    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+    <div className={`modal ${activeModal === "preview" ? "modal_opened" : ""}`}>
       <div className="modal__content modal__content_type_image">
         <button
           onClick={onClose}
